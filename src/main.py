f5115e045374697c9df527304f6b4e140aa2b837
Ernest M. <git@wtvmail.com>
2025-01-16 14:58:48 +0200

    Tried creating a paddle object but ran into a bug. Used a dictionary instead

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from scripts.display_resolution import Display_resolution
 from scripts.constants import *
-from scripts.entities import Ball, Paddle
+from scripts.entities import Ball, paddle
 
 class Game:
     def __init__(self):
@@ -10,31 +10,34 @@ class Game:
         pygame.init()
         if not pygame.get_init():
             raise RuntimeError("Pygame failed to initialize")
-
+        
+        # Creating the window and the game surface that will be blitted to the window
         self.__res = Display_resolution()
         self.__screen = pygame.display.set_mode(self.__res.get_screen_size(), pygame.SCALED + pygame.FULLSCREEN + pygame.NOFRAME)
         pygame.display.set_caption("Table Tennis")
-        self.__clock = pygame.time.Clock()
         # self.ICON = pygame.image.load("PATH")
         # pygame.display.set_icon(self.ICON)
-
         self.__game_surface = pygame.Surface(self.__res.get_screen_size())
 
-        self.__left_paddle = Paddle()
+        self.__clock = pygame.time.Clock()
+        
+        # Creating the two paddles
+        self.__left_paddle = paddle(self.__res)
+        self.__right_paddle = paddle(self.__res, False)
 
     def main_menu(self):
         pass
 
     def run(self):
         while True:
-            # self.__ball.draw()
-            pygame.draw.rect(self.__game_surface, WHITE, self.__left_paddle.get_rect())
+            pygame.draw.rect(self.__game_surface, WHITE, self.__left_paddle["rect"])
+            pygame.draw.rect(self.__game_surface, WHITE, self.__right_paddle["rect"])
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-            self.__screen.blit(pygame.transform.scale(self.__game_surface, self.__resolution.get_screen_size()), (0, 0))
+            self.__screen.blit(pygame.transform.scale(self.__game_surface, self.__res.get_screen_size()), (0, 0))
             pygame.display.update()
             self.__clock.tick(FPS)
 
--- src/scripts/entities.py
+++ src/scripts/entities.py
@@ -2,90 +2,33 @@ import pygame
 from scripts.constants import *
 from scripts.display_resolution import Display_resolution
 
-class Paddle():
-    # def __init__(self, left: int, top: int, width: int, height: int, screen_width: int, screen_height: int, left_paddle: bool = True):
-    #     # Call the parent constructor to initialize the rectangle
-    #     super().__init__(left, top, width, height)
+def paddle(display_resolution_object: Display_resolution, left_paddle: bool = True) -> dict:
+    """
+    Returns a dictionary containing the paddle's width, height, x and y coordinates and its rect object.
+    """
+    resolution = display_resolution_object
+    width: int = resolution.scaled_down(PADDLE_WIDTH)
+    height: int = resolution.get_screen_height()//5
 
-    #     self.__res = Display_resolution()
-        
-    #     self.__width: int = self.__res.scaled_down(PADDLE_WIDTH)
-    #     self.__height: int = self.__res.get_screen_height()//5
+    if left_paddle:
+        x_position: int = resolution.scaled_down(DISTANCE_FROM_HEIGHT)
+    else:
+        x_position: int = resolution.get_screen_width() - resolution.scaled_down(DISTANCE_FROM_HEIGHT) - width
 
-    #     if left_paddle:
-    #         self.__x_position: int = self.__res.scaled_down(DISTANCE_FROM_HEIGHT)
-    #     else:
-    #         self.__x_position: int = self.__res.get_screen_width() - self.__res.scaled_down(DISTANCE_FROM_HEIGHT) - self.__width
-        
-    #     # The paddle height is 1/5 of the screen height, so the y position will be in the middle, at the 3rd position
-    #     self.__y_position: int = self.__height * 2
+    # The paddle height is 1/5 of the screen height, so the y position will be in the middle, at the 3rd position
+    y_position: int = height * 2
 
-    # def x(self, x: int =):
+    rect: pygame.Rect = pygame.Rect(x_position, y_position, width, height)
 
-    def __init__(self, left_paddle: bool = True):
-        if not pygame.get_init(): # Check if Pygame is already initialized
-            pygame.init()
-            
-        self.__res = Display_resolution()
-        self.__width: int = self.__res.scaled_down(PADDLE_WIDTH)
-        self.__height: int = self.__res.get_screen_height()//5
+    paddle_infos = {
+        "width": width,
+        "height": height,
+        "x": x_position,
+        "y": y_position,
+        "rect": rect
+    }
 
-        if left_paddle:
-            self.__x_position: int = self.__res.scaled_down(DISTANCE_FROM_HEIGHT)
-        else:
-            self.__x_position: int = self.__res.get_screen_width() - self.__res.scaled_down(DISTANCE_FROM_HEIGHT) - self.__width
-        
-        # The paddle height is 1/5 of the screen height, so the y position will be in the middle, at the 3rd position
-        self.__y_position: int = self.__height * 2
-        
-        self.__rect: pygame.Rect = pygame.Rect(self.__x_position, self.__y_position, self.__width, self.__height)
-
-    def y(self, y: int) -> None:
-        """
-        Set the new y postion of the paddle
-        """
-        self.__y_position = y
-
-    def get_x(self) -> int:
-        """
-        Returns the x position of the paddle
-        """
-        x = self.__x_position
-        
-        return x
-
-    def get_y(self) -> int:
-        """
-        Returns the y position of the paddle
-        """
-        y = self.__y_position
-
-        return y
-    
-    def get_width(self) -> int:
-        """
-        Returns the width of the paddle
-        """
-        width = self.__width
-        
-        return width
-    
-    def get_height(self) -> int:
-        """
-        Returns the height of the paddle
-        """
-        height = self.__height
-        
-        return height
-    
-    def get_rect(self) -> pygame.Rect:
-        """
-        Returns the rect object of the paddle
-        """
-        rect = self.__rect
-
-        return rect
-   
+    return paddle_infos
 
 class Ball:
     def __init__(self, surface: pygame.Surface, screen_width: int, screen_height: int):
