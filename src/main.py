a97e7f1bd6912a8dccac9a3e43e82bb58100f363
Ernest M. <git@wtvmail.com>
2025-01-19 14:37:14 +0200

    Created the paddle movement in the game loop

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from scripts.display_resolution import Display_resolution
 from scripts.constants import *
-from scripts.entities import paddle, draw_paddle, ball
+from scripts.entities import paddle, draw_paddle, ball, draw_ball
 
 class Game:
     def __init__(self):
@@ -28,11 +28,36 @@ class Game:
         # Creating the ball
         self.__ball = ball(self.__res)
 
-        # The turn variable will contain either the left paddle of right paddle. By default it's the left.
-        self.__turn = self.__left_paddle
-
     def main_menu(self):
         pass
+
+    def __move_left_paddle(self):
+        keys = pygame.key.get_pressed()
+
+        up_key = pygame.K_w
+        down_key = pygame.K_s
+
+        # Checking if he's not trying to go outside of the top screen
+        if keys[up_key] and self.__left_paddle["y"] > self.__res.scaled_down(MIN_DISTANCE_FROM_TOP):
+            self.__left_paddle["y"] -= self.__res.scaled_down(PADDLE_SPEED)
+
+        # Checking if he's not trying to go outside of the bottom screen
+        if keys[down_key] and self.__left_paddle["y"] < self.__res.scaled_down(MIN_DISTANCE_FROM_BOTTOM): 
+            self.__left_paddle["y"] += self.__res.scaled_down(PADDLE_SPEED)
+
+    def __move_right_paddle(self):
+        keys = pygame.key.get_pressed()
+
+        up_key = pygame.K_UP
+        down_key = pygame.K_DOWN
+
+        # Checking if he's not trying to go outside of the top screen
+        if keys[up_key] and self.__right_paddle["y"] > self.__res.scaled_down(MIN_DISTANCE_FROM_TOP):
+            self.__right_paddle["y"] -= self.__res.scaled_down(PADDLE_SPEED)
+
+        # Checking if he's not trying to go outside of the bottom screen
+        if keys[down_key] and self.__right_paddle["y"] < self.__res.scaled_down(MIN_DISTANCE_FROM_BOTTOM): 
+            self.__right_paddle["y"] += self.__res.scaled_down(PADDLE_SPEED)
 
     def run(self):
 
@@ -43,7 +68,7 @@ class Game:
             # Drawing the paddles and the ball on the game surface
             draw_paddle(self.__game_surface, WHITE, self.__left_paddle)
             draw_paddle(self.__game_surface, WHITE, self.__right_paddle)
-            pygame.draw.circle(self.__game_surface, WHITE, (self.__ball["x"], self.__ball["y"]), self.__ball["radius"])
+            draw_ball(self.__game_surface, WHITE, self.__ball)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -51,12 +76,9 @@ class Game:
                     sys.exit()
 
             # Here we will handle the up and down movements of the paddle. It's outside the event loop because it's a continuous action.
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP] and self.__turn["y"] > self.__res.scaled_down(MIN_DISTANCE_FROM_TOP):
-                self.__turn["y"] -= self.__res.scaled_down(PADDLE_SPEED)
-            if keys[pygame.K_DOWN] and self.__turn["y"] < self.__res.scaled_down(MIN_DISTANCE_FROM_BOTTOM): 
-                self.__turn["y"] += self.__res.scaled_down(PADDLE_SPEED)
-
+            self.__move_left_paddle()
+            self.__move_right_paddle()
+            
             self.__screen.blit(pygame.transform.scale(self.__game_surface, self.__res.get_screen_size()), (0, 0))
             pygame.display.update()
             self.__clock.tick(FPS)
--- src/scripts/entities.py
+++ src/scripts/entities.py
@@ -29,14 +29,18 @@ def paddle(display_resolution_object: Display_resolution, left_paddle: bool = Tr
 
     return paddle_infos
 
+
 def draw_paddle(surface: pygame.Surface, color: tuple, paddle: dict) -> None:
-   
+    """
+    Draw the paddle on a given surface
+    """
     paddle["rect"] = pygame.Rect(paddle["x"], paddle["y"], paddle["width"], paddle["height"])
     pygame.draw.rect(surface, color, paddle["rect"])
 
+
 def ball(display_resolution_object: Display_resolution) -> dict:
     """
-    Returns a dictionary containing the ball's x and y coordinates and its radius.
+    Returns a dictionary containing the ball's center position, x and y coordinates (of the center) and its radius.
     """
     resolution = display_resolution_object
     x_position: int = resolution.get_screen_width() //2
@@ -46,10 +50,19 @@ def ball(display_resolution_object: Display_resolution) -> dict:
     ball_infos = {
         "x": x_position,
         "y": y_position,
+        "center": (x_position, y_position),
         "radius": radius
     }
 
     return ball_infos
+
+
+def draw_ball(surface: pygame.Surface, color: tuple, ball: dict) -> None:
+    """
+    Draw the ball on a given surface
+    """
+    pygame.draw.circle(surface, color, ball["center"], ball["radius"])
+    
 
 if __name__ == "__main__":
     print("This script is only meant to be imported")
