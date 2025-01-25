d2292b353ec63b374cb9a1e7e3c0491a4176d165
Ernest M. <180271155+ernest-mm@users.noreply.github.com>
2025-01-25 18:17:35 +0200

    Started implementing the ball's movements

--- src/main.py
+++ src/main.py
@@ -31,16 +31,16 @@ class Game:
     def main_menu(self):
         pass
 
-    def __paddle_inside_screen(self, y_coordinate: int, velocity: int, up_direction: bool = True) -> bool:
+    def __paddle_inside_screen(self, y_coordinate: int, speed: int, up_direction: bool = True) -> bool:
         """
         Returns True if for the next movement, the paddle will be inside the screen. 
         """
 
         if up_direction:
-            if (y_coordinate - velocity) > self.__res.scaled_down(MIN_DISTANCE_FROM_TOP):
+            if (y_coordinate - speed) > self.__res.scaled_down(MIN_DISTANCE_FROM_TOP):
                 return True
         else:
-            if y_coordinate + velocity < self.__res.scaled_down(MIN_DISTANCE_FROM_BOTTOM):
+            if y_coordinate + speed < self.__res.scaled_down(MIN_DISTANCE_FROM_BOTTOM):
                 return True
             
         return False
@@ -63,15 +63,19 @@ class Game:
 
         # Moving the paddles only if they are inside the screen
 
-        if keys[left_paddle_up] and self.__paddle_inside_screen(self.__left_paddle["y"], self.__left_paddle["velocity"]):
-            self.__left_paddle["y"] -= self.__left_paddle["velocity"]
-        if keys[left_paddle_down] and self.__paddle_inside_screen(self.__left_paddle["y"], self.__left_paddle["velocity"], False):
-            self.__left_paddle["y"] += self.__left_paddle["velocity"]
+        if keys[left_paddle_up] and self.__paddle_inside_screen(self.__left_paddle["y"], self.__left_paddle["speed"]):
+            self.__left_paddle["y"] -= self.__left_paddle["speed"]
+        if keys[left_paddle_down] and self.__paddle_inside_screen(self.__left_paddle["y"], self.__left_paddle["speed"], False):
+            self.__left_paddle["y"] += self.__left_paddle["speed"]
 
-        if keys[right_paddle_up] and self.__paddle_inside_screen(self.__right_paddle["y"], self.__right_paddle["velocity"]):
-            self.__right_paddle["y"] -= self.__right_paddle["velocity"]
-        if keys[right_paddle_down] and self.__paddle_inside_screen(self.__right_paddle["y"], self.__right_paddle["velocity"], False):
-            self.__right_paddle["y"] += self.__right_paddle["velocity"]
+        if keys[right_paddle_up] and self.__paddle_inside_screen(self.__right_paddle["y"], self.__right_paddle["speed"]):
+            self.__right_paddle["y"] -= self.__right_paddle["speed"]
+        if keys[right_paddle_down] and self.__paddle_inside_screen(self.__right_paddle["y"], self.__right_paddle["speed"], False):
+            self.__right_paddle["y"] += self.__right_paddle["speed"]
+
+    def __ball_movements(self):
+        self.__ball["x"] += self.__ball["x_speed"]
+        self.__ball["y"] -= self.__ball["y_speed"]
 
     def run(self):
 
@@ -91,6 +95,9 @@ class Game:
 
             # Here we will handle the up and down movements of the paddle. It's outside the event loop because it's a continuous action.
             self.__paddles_movements()
+
+            # Here we will handle the ball's movements
+            self.__ball_movements()
             
             self.__screen.blit(pygame.transform.scale(self.__game_surface, self.__res.get_screen_size()), (0, 0))
             pygame.display.update()
--- src/scripts/entities.py
+++ src/scripts/entities.py
@@ -4,7 +4,7 @@ from scripts.display_resolution import Display_resolution
 
 def paddle(display_resolution_object: Display_resolution, left_paddle: bool = True) -> dict:
     """
-    Returns a dictionary containing the paddle's width, height, x and y coordinates and velocity.
+    Returns a dictionary containing the paddle's width, height, x and y coordinates and speed.
     """
     resolution = display_resolution_object
     width: int = resolution.scaled_down(PADDLE_WIDTH)
@@ -18,14 +18,14 @@ def paddle(display_resolution_object: Display_resolution, left_paddle: bool = Tr
     # The paddle height is 1/5 of the screen height, so the y position will be in the middle, at the 3rd position
     y_position: int = height * 2
 
-    velocity = resolution.scaled_down(PADDLE_VELOCITY)
+    speed = resolution.scaled_down(PADDLE_SPEED)
 
     paddle_infos = {
         "width": width,
         "height": height,
         "x": x_position,
         "y": y_position,
-        "velocity": velocity
+        "speed": speed
     }
 
     return paddle_infos
@@ -41,7 +41,7 @@ def draw_paddle(surface: pygame.Surface, color: tuple, paddle: dict) -> None:
 
 def ball(display_resolution_object: Display_resolution) -> dict:
     """
-    Returns a dictionary containing the ball's center position, x and y coordinates (of the center) and its radius.
+    Returns a dictionary containing the ball's x and y coordinates (they are the center of the ball), x's and y's speed and the radius.
     """
     resolution = display_resolution_object
     x_position: int = resolution.get_screen_width() //2
@@ -51,8 +51,9 @@ def ball(display_resolution_object: Display_resolution) -> dict:
     ball_infos = {
         "x": x_position,
         "y": y_position,
-        "center": (x_position, y_position),
-        "radius": radius
+        "radius": radius,
+        "x_speed": resolution.scaled_down(30),
+        "y_speed": 0
     }
 
     return ball_infos
@@ -62,7 +63,7 @@ def draw_ball(surface: pygame.Surface, color: tuple, ball: dict) -> None:
     """
     Draw the ball on a given surface
     """
-    pygame.draw.circle(surface, color, ball["center"], ball["radius"])
+    pygame.draw.circle(surface, color, (ball["x"], ball["y"]), ball["radius"])
     
 
 if __name__ == "__main__":
